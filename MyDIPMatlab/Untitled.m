996e3433b07a2bb6450371ce3ad474bf751ae7ef
linchao <linchao.hide@gmail.com>
2018-11-25 19:31:38 +0800

    除小孩图片，其他基本完成，准备写报告

--- MyDIPMatlab/Untitled.m
+++ MyDIPMatlab/Untitled.m
@@ -1,31 +1,22 @@
 clear;
 clc;
-%imageOrigin = imread('..\test\test5.jpg');
-i=imread('..\test\test5.jpg');
-i=rgb2gray(i);
-%添加噪声
-ig=imnoise(i,'poisson');
-%获取算子
-s=GetStrelList();
-%串联去噪
-edit erodeList
-e=erodeList(ig,s);
-edit getRateList
-%计算权重
-f=getRateList(ig,e);
-edit getRemoveResult
-%并联
-igo=getRemoveResult(f,e);
-%显示结果
-subplot(1,2,1),imshow(f);
-subplot(1,2,1),imshow(i);
-title('原图像');
-subplot(1,2,2),imshow(ig),title('噪声图像');
-figure;
-subplot(2,2,1),imshow(e.eroded_co12);title('串联1处理结果');
-subplot(2,2,2),imshow(e.eroded_co22);title('串联2处理结果');
-subplot(2,2,3),imshow(e.eroded_co32);title('串联3处理结果');
-subplot(2,2,4),imshow(e.eroded_co42);title('串联4处理结果');
-figure;
-subplot(1,2,1),imshow(ig),title('噪声图像');
-subplot(1,2,2),imshow(igo),title('并联去噪图像');
+numSingle = ones(1,1000);
+i = 2;
+index = 1;
+p = 18;
+while i < 2^p
+    if isprime(i) == 1
+        numSingle(index) = i;
+        index = index + 1;
+    end
+    i = i + 1;
+end
+bin = dec2bin(numSingle);
+numS = ones(p,1);
+numS2 = ones(p,1);
+numS3 = ones(p,1);
+for i = 1:p
+    numS(i) = sum(numSingle<2^i & numSingle>=2^(i-1));
+    numS2(i) = sum(numSingle<=2^i & numSingle>2^(i-1));
+    numS3(i) = 2^(i-1);
+end
